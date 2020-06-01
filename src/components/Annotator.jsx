7ad97d51e4345911f785824a67176f0e2a22a73f
Michael Owens <mowens@ucdavis.edu>
2020-06-01 05:54:53 -0700

    selection and text highlighting

--- src/components/Annotator.jsx
+++ src/components/Annotator.jsx
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Document from './Document';
 import styles from '../styles/Annotator.module.css';
 
 function Annotator({file}) {
-  const {container, annotations, document_styles} = styles;
+  const [annotationsMap, setAnnotationsMap] = useState({});
+  const {container, annotations_container, document} = styles;
+
+  const highlightSelectedText = () => {
+    const selection = window.getSelection();
+    if (selection) {
+      console.log(selection.anchorNode.parentElement)
+      // const parent = selection.anchorNode.parentElement.parentElement;
+      const parent = selection.anchorNode.parentElement;
+      const range = selection.getRangeAt(0);
+      if (range) {
+        const contents = range.extractContents();
+        const nodes = contents.querySelectorAll('*');
+        let newContent = new DocumentFragment();
+        for (let n of nodes) {
+          n.style.color = 'red';
+          newContent.appendChild(n);
+        }
+        parent.append(newContent);
+      }
+    }
+  }
+
+
+  const tempStyle = {
+    position: 'absolute',
+    top: '423.888916015625px',
+  }
+
   return (
     <div className={container}>
-      <span className={document_styles}><Document file={file} /></span>
-      <div className={annotations}>Annotations will go here</div>
+      <span className={document}><Document file={file} /></span>
+      <div className={annotations_container}>
+        <button onClick={highlightSelectedText}>highlight selection</button>
+        Annotations will go here
+        <div id="test" style={tempStyle}> proof of concept</div>
+      </div>
     </div>
   )
 }
+
+
 
 export default Annotator;
