bec073842c8f4362aa4b6c60a6035a97a245c7c0
Michael Owens <mowens@ucdavis.edu>
2020-06-12 02:29:15 -0700

    in progress

--- src/components/Annotator.jsx
+++ src/components/Annotator.jsx
@@ -9,19 +9,34 @@ function Annotator({file}) {
   const highlightSelectedText = () => {
     const selection = window.getSelection();
     if (selection) {
-      console.log(selection.anchorNode.parentElement)
+      console.log('rangeCount: ', selection.rangeCount)
+      // console.log('parent')
+      // console.log(selection.anchorNode.parentElement)
+      // console.log('grandparent')
+      // console.log(selection.anchorNode.parentElement.parentElement)
       // const parent = selection.anchorNode.parentElement.parentElement;
       const parent = selection.anchorNode.parentElement;
       const range = selection.getRangeAt(0);
       if (range) {
-        const contents = range.extractContents();
-        const nodes = contents.querySelectorAll('*');
-        let newContent = new DocumentFragment();
-        for (let n of nodes) {
-          n.style.color = 'red';
-          newContent.appendChild(n);
-        }
-        parent.append(newContent);
+        debugger;
+        console.log('start container parentNode:')
+        console.log(range.startContainer.parentNode)
+        console.log('end container parentNode:')
+        console.log(range.endContainer.parentNode)
+        
+        // debugger;
+        // console.log('start: ')
+        // console.log(range.startContainer);
+        // console.log('end: ')
+        // console.log(range.endContainer);
+        // const contents = range.extractContents();
+        // const nodes = contents.querySelectorAll('*');
+        // let newContent = new DocumentFragment();
+        // for (let n of nodes) {
+        //   n.style.color = 'red';
+        //   newContent.appendChild(n);
+        // }
+        // parent.append(newContent);
       }
     }
   }
@@ -29,16 +44,19 @@ function Annotator({file}) {
 
   const tempStyle = {
     position: 'absolute',
-    top: '423.888916015625px',
+    // top: '423.888916015625px',
+    top: '20px',
   }
 
   return (
     <div className={container}>
-      <span className={document}><Document file={file} /></span>
+      <span ><Document file={file} /></span>
       <div className={annotations_container}>
-        <button onClick={highlightSelectedText}>highlight selection</button>
+        
         Annotations will go here
-        <div id="test" style={tempStyle}> proof of concept</div>
+        <div id="test" style={tempStyle}> proof of concept
+          <button onClick={highlightSelectedText}>highlight selection</button>
+        </div>
       </div>
     </div>
   )
